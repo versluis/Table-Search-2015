cdf8f0b8895c5e3d802f0175be7144db09a864cf
Jay Versluis <jay@versluis.com>
2015-11-05 18:12:14 -0500

    added search controller and predicate

--- Table Search/MainTableViewController.m
+++ Table Search/MainTableViewController.m
@@ -7,15 +7,22 @@
 //
 
 #import "MainTableViewController.h"
+#import "SearchResultsViewController.h"
 
-@interface MainTableViewController ()
+@interface MainTableViewController () <UISearchResultsUpdating>
 @property (strong, nonatomic) NSMutableArray *data;
+@property (strong, nonatomic) UISearchController *controller;
+@property (strong, nonatomic) NSArray *results;
 @end
 
 @implementation MainTableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    SearchResultsViewController *searchResults = (SearchResultsViewController *)self.controller.searchResultsController;
+    [self addObserver:searchResults forKeyPath:@"results" options:NSKeyValueObservingOptionNew context:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,13 +38,29 @@
         NSNumberFormatter *formatter = [[NSNumberFormatter alloc]init];
         formatter.numberStyle = NSNumberFormatterSpellOutStyle;
         
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 3000; i++) {
             NSNumber *thisNumber = [NSNumber numberWithInt:i];
             NSString *spelledOutNumber = [formatter stringFromNumber:thisNumber];
             [_data addObject:spelledOutNumber];
         }
     }
     return _data;
+}
+
+- (UISearchController *)controller {
+    
+    if (!_controller) {
+        
+        // instantiate search results table view
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        SearchResultsViewController *resultsController = [storyboard instantiateViewControllerWithIdentifier:@"SearchResults"];
+        
+        // create search controller
+        _controller = [[UISearchController alloc]initWithSearchResultsController:resultsController];
+        _controller.searchResultsUpdater = self;
+        
+    }
+    return _controller;
 }
 
 #pragma mark - Table view data source
@@ -60,6 +83,31 @@
     
     return cell;
 }
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+# pragma - Search Results Updater
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
+    
+    // filter the search results
+    // self.results = nil;
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF contains [cd] %@", self.controller.searchBar.text];
+    self.results = [self.data filteredArrayUsingPredicate:predicate];
+    
+    NSLog(@"Search Results are: %@", [self.results description]);
+}
+
+- (IBAction)searchButtonPressed:(id)sender {
+    
+    // present the search controller
+    [self presentViewController:self.controller animated:YES completion:nil];
+
+}
+
 
 
 
